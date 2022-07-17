34951efdaed80ae67e6c8adc658eee9eb57f2cf1
Mykhailo Kurtiak <mikhaylokurtyak@gmail.com>
2022-07-17 17:12:04 +0200

    second commit

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': '9c052cafdfmsh122d98e2ab636f2p1fde20jsn4633259d437f',
+    'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com',
+  },
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+
+export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5';
+export const WEATHER_API_KEY = '957031efadaee9de14092aceeb6b3ca7';
